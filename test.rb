1e89d1d5def4ef9861d9f744ae81fcbb459b0d91
Tyler Long <tyler4long@gmail.com>
2015-04-19 10:57:29 +0800

    Initial commit

---
+++ test.rb
@@ -0,0 +1,48 @@
+def plus(second)
+  return lambda{ |first| first + second }
+end
+def minus(second)
+  return lambda{ |first| first - second }
+end
+def times(second)
+  return lambda{ |first| first * second }
+end
+def divided_by(second)
+  return lambda{ |first| first / second }
+end
+
+def zero(func = nil)
+  func.nil? ? 0 : func.call(zero)
+end
+def one(func = nil)
+  func.nil? ? 1 : func.call(one)
+end
+def two(func = nil)
+  func.nil? ? 2 : func.call(two)
+end
+def three(func = nil)
+  func.nil? ? 3 : func.call(three)
+end
+def four(func = nil)
+  func.nil? ? 4 : func.call(four)
+end
+def five(func = nil)
+  func.nil? ? 5 : func.call(five)
+end
+def six(func = nil)
+  func.nil? ? 6 : func.call(six)
+end
+def seven(func = nil)
+  func.nil? ? 7 : func.call(seven)
+end
+def eight(func = nil)
+  func.nil? ? 8 : func.call(eight)
+end
+def nine(func = nil)
+  func.nil? ? 9 : func.call(nine)
+end
+
+puts seven(times(five))
+puts four(plus(nine))
+puts eight(minus(three))
+puts six(divided_by(two))
